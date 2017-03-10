528537f2b74bb0792e1bffb7b8d21575ffdb9afd
Adam Schubert <adam.schubert@sg1-game.net>
2017-03-10 19:14:47 +0100

    use mb_strlen for note len check

--- src/Model/Package.php
+++ src/Model/Package.php
@@ -148,7 +148,7 @@ class Package
      */
     public function setNote($note = null)
     {
-        if (strlen($note) > 300) {
+        if (mb_strlen($note) > 300) {
             throw new WrongDataException('$note is longer then 300 characters');
         }
 
