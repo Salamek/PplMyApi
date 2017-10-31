b2e05e3693e1c254073590b9b25c5e65923708d7
Adam Schubert <adam.schubert@sg1-game.net>
2017-10-31 19:28:05 +0100

    SSL is valid, BUG1 removed

--- src/Api.php
+++ src/Api.php
@@ -19,9 +19,8 @@ use Salamek\PplMyApi\Model\PickUpOrder;
  * Class Client
  * @package Salamek
  * ######## BUGS ###########
- * 1) SSL IS NOT VALID
- * 2) API SHOULD GENERATE ID
- * 3) API SHOULD GENERATE LABELS
+ * 1) API SHOULD GENERATE ID
+ * 2) API SHOULD GENERATE LABELS
  *
  * ######### BUGS IN DOC #######
  * isHealtly:
