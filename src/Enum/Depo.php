1e16c511a5eb44eda89e65fd860cdcef47a9552f
Alex Krátký <33813757+AlexKratky@users.noreply.github.com>
2021-09-16 14:19:33 +0200

    added more depos codes
    on https://www.ppl.cz/prepravni-sit-ppl is information that there is actually 24 depos, not only 13

--- src/Enum/Depo.php
+++ src/Enum/Depo.php
@@ -20,7 +20,18 @@ class Depo
     const CODE_10 = '10';
     const CODE_11 = '11';
     const CODE_12 = '12';
+    const CODE_13 = '13';
     const CODE_14 = '14';
+    const CODE_15 = '15';
+    const CODE_16 = '16';
+    const CODE_17 = '17';
+    const CODE_18 = '18';
+    const CODE_19 = '19';
+    const CODE_20 = '20';
+    const CODE_21 = '21';
+    const CODE_22 = '22';
+    const CODE_23 = '23';
+    const CODE_24 = '24';
 
     /** @var array */
     public static $list = [
@@ -36,6 +47,17 @@ class Depo
         self::CODE_10,
         self::CODE_11,
         self::CODE_12,
-        self::CODE_14
+        self::CODE_13,
+        self::CODE_14,
+        self::CODE_15,
+        self::CODE_16,
+        self::CODE_17,
+        self::CODE_18,
+        self::CODE_19,
+        self::CODE_20,
+        self::CODE_21,
+        self::CODE_22,
+        self::CODE_23,
+        self::CODE_24
     ];
 }
