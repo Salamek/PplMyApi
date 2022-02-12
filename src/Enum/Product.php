4ea2be6a3480c5fb120807f60fdb1a1982ff59e3
Adam Schubert <adam.schubert@sg1-game.net>
2022-02-12 10:07:58 +0100

    feat(Enum.Product): Added more products from NEW DOCs

--- src/Enum/Product.php
+++ src/Enum/Product.php
@@ -17,12 +17,17 @@ class Product
     const PPL_PARCEL_IMPORT = 11;
     const PPL_PARCEL_CZ_PRIVATE = 13;
     const PPL_PARCEL_CZ_PRIVATE_COD = 14;
-    const COMPANY_PALETTE = 15;
-    const COMPANY_PALETTE_COD = 16;
-    const PRIVATE_PALETTE = 19;
-    const PRIVATE_PALETTE_COD = 20;
+    const COMPANY_PALETTE = 15; // !FIXME this one is missing in the DOC PDF
+    const COMPANY_PALETTE_COD = 16; // !FIXME this one is missing in the DOC PDF
+    const PRIVATE_PALETTE = 19; // !FIXME this one is missing in the DOC PDF
+    const PRIVATE_PALETTE_COD = 20; // !FIXME this one is missing in the DOC PDF
     const PPL_PARCEL_CONNECT = 36;
     const PPL_PARCEL_CONNECT_COD = 37;
+    const PPL_PARCEL_CZ_RETURN = 45;
+    const PPL_PARCEL_CZ_SMART = 46;
+    const PPL_PARCEL_CZ_SMART_COD = 47;
+    const PPL_PARCEL_SMART_EUROPE = 48;
+    const PPL_PARCEL_SMART_EUROPE_COD = 49;
 
     /** @var array */
     public static $list = [
@@ -39,7 +44,12 @@ class Product
         self::PRIVATE_PALETTE,
         self::PRIVATE_PALETTE_COD,
         self::PPL_PARCEL_CONNECT,
-        self::PPL_PARCEL_CONNECT_COD
+        self::PPL_PARCEL_CONNECT_COD,
+        self::PPL_PARCEL_CZ_RETURN,
+        self::PPL_PARCEL_CZ_SMART,
+        self::PPL_PARCEL_CZ_SMART_COD,
+        self::PPL_PARCEL_SMART_EUROPE,
+        self::PPL_PARCEL_SMART_EUROPE_COD
     ];
 
     public static $names = [
@@ -52,7 +62,12 @@ class Product
         self::PPL_PARCEL_CZ_PRIVATE => 'PPL Parcel CZ Private',
         self::PPL_PARCEL_CZ_PRIVATE_COD => 'PPL Parcel CZ Private - dobírka',
         self::PPL_PARCEL_CONNECT => 'PPL Parcel Connect',
-        self::PPL_PARCEL_CONNECT_COD=> 'PPL Parcel Connect – dobírka'
+        self::PPL_PARCEL_CONNECT_COD=> 'PPL Parcel Connect – dobírka',
+        self::PPL_PARCEL_CZ_RETURN=> 'PPL Parcel Return CZ',
+        self::PPL_PARCEL_CZ_SMART=> 'PPL Parcel CZ Smart',
+        self::PPL_PARCEL_CZ_SMART_COD=> 'PPL Parcel CZ Smart – dobírka',
+        self::PPL_PARCEL_SMART_EUROPE=> 'PPL Parcel Smart Europe',
+        self::PPL_PARCEL_SMART_EUROPE_COD=> 'PPL Parcel Smart Europe – dobírka'
     ];
 
     public static $cashOnDelivery = [
@@ -62,7 +77,9 @@ class Product
         self::PPL_PARCEL_CZ_PRIVATE_COD,
         self::COMPANY_PALETTE_COD,
         self::PRIVATE_PALETTE_COD,
-        self::PPL_PARCEL_CONNECT_COD
+        self::PPL_PARCEL_CONNECT_COD,
+        self::PPL_PARCEL_CZ_SMART_COD,
+        self::PPL_PARCEL_SMART_EUROPE_COD
     ];
 
     public static $deliverySaturday = [
@@ -73,7 +90,11 @@ class Product
         self::PPL_PARCEL_CZ_AFTERNOON_PACKAGE,
         self::PPL_PARCEL_CZ_AFTERNOON_PACKAGE_COD,
         self::EXPORT_PACKAGE,
-        self::EXPORT_PACKAGE_COD
+        self::EXPORT_PACKAGE_COD,
+        self::PPL_PARCEL_CZ_SMART,
+        self::PPL_PARCEL_CZ_SMART_COD,
+        self::PPL_PARCEL_SMART_EUROPE,
+        self::PPL_PARCEL_SMART_EUROPE_COD
     ];
 
     public static $privateProducts = [
